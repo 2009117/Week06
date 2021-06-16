34519c9fd2c6e8512d43170fed90bd8a95c1a97b
2009117 <2009117@upy.edu.mx>
2021-06-15 19:49:12 -0500

    Week06

---
+++ Chapter4.ex5Chris.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void){
+float f;
+f=25;
+
+printf("X is equal to %d\n", f);
+
+int I;
+I=30;
+
+printf("Z is equal to %f", I);
+
+
+char C='B';
+printf("Character is equal to %d", c)
+
+}
